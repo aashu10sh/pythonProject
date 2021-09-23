737340f1912e2fae7c054a42ba2ff3f8f327dbe9
ashshelby <nepalidude3@gmail.com>
2021-09-23 21:29:57 +0545

    qol changes

--- shop/models.py
+++ shop/models.py
@@ -11,13 +11,16 @@ class Shop(models.Model):
     contact_person = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     image = models.ImageField(default='test.jpeg', null=True, upload_to='event/')
-    
+    def __str__(self):
+        return self.shop_name
 
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
     price = models.IntegerField()
     discount = models.FloatField()
     old_price = models.IntegerField()
+    def __str__(self):
+        return self.product_name
     
 
 class Deal(models.Model):
@@ -26,3 +29,6 @@ class Deal(models.Model):
     valid_till = models.DateField()
     discount_percent = models.FloatField()
     done_by = models.ForeignKey(Shop,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.deal_name
